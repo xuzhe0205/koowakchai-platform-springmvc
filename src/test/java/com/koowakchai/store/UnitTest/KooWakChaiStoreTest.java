b83e833024144c509303af5f82553e850f6e4ebd
Zhe Xu <zhexu@Zhes-MacBook-Pro.local>
2020-04-05 17:07:34 -0400

    jar

--- src/test/java/com/koowakchai/store/UnitTest/KooWakChaiStoreTest.java
+++ src/test/java/com/koowakchai/store/UnitTest/KooWakChaiStoreTest.java
@@ -79,108 +79,108 @@ public class KooWakChaiStoreTest {
     @Autowired
     private TECigaretteDao teCigaretteDao;
 
-//    @Test
-//    public void testGetAllBusinessTypes() throws Exception{
-//        List<String> businessTypeList = tBusinessService.getAllBusinessTypes();
-//        System.out.println("test hahah: " + businessTypeList.size());
-//    }
-//
-//    @Test
-//    public void testGetUsers() throws Exception{
-//        Long id = tUserDao.getUserId("oliverxu0205@gmail.com");
-//        System.out.println("test hahah: " + id);
-//    }
-//
-//    @Test
-//    public void testLoginToken() throws Exception{
-//        List<Object> userInfoEntity = tUserService.getUserInfo("xuzhe3", "950205", "driver");
-//        String token = "";
-//        if (userInfoEntity != null) {
-//            Object[] resultUserInfo = (Object[]) userInfoEntity.get(0);
-//            Long userId = Long.parseLong(String.valueOf(resultUserInfo[0]));
-//            token = JWTUtils.createToken(userId);
-//            Map<String, Claim> claims = jwtUtils.verifyToken(token);
-//        }
-//
-//    }
-//
-//    @Test
-//    public void testGetUserId() throws Exception{
-//        Long userId = tUserDao.getUserId("oliverxu0205@gmail.com");
-//        System.out.println("your userId: " + userId);
-//    }
-//
-//    @Test
-//    public void testShoppingCartItems() throws Exception{
-//        int cartSize = tShoppingCartService.getCartItem(1).size();
-//        System.out.println("my shopping cart size: " + cartSize);
-//    }
-//
-//    @Test
-//    public void testEmail() throws Exception{
-////        List<Object> objectList = new ArrayList<>();
-////        TTotalOrderEntity tTotalOrderEntity = tTotalOrderDao.getTTotalOrderEntity(2);
-////        objectList.add(tTotalOrderEntity);
-//        List<Long> orderIds = new ArrayList<>();
-//        long l1 = 25;
-//        long l2= 26;
-//        orderIds.add(l1);
-//        orderIds.add(l2);
-////        TTotalOrderEntity tTotalOrderEntity = tTotalOrderDao.getTTotalOrderEntity(l1);
-//        System.out.println("sent email: " + storeEmailService.sendConfirmationEmail(orderIds));
-//
-//    }
-//
-//    @Test
-//    public void testLogisticsOrder() throws Exception{
-//        List<Long> orderIds = new ArrayList<>();
-//        orderIds.add(new Long(25));
-//        orderIds.add(new Long(26));
-//        tLogisticsOrderService.addShippingOrders(orderIds,1);
-//
-//    }
-//
-//    @Test
-//    public void testGetTAirportInfoByCondition() throws Exception{
-//        List<TAirportInfoEntity> tAirportInfoEntityList = tAirportRideDao.getTAirportEntityByCondition("", "");
-//        System.out.println("airportinfo number: " + tAirportInfoEntityList.size());
-//
-//    }
-//
-//    @Test
-//    public void testGetDriverByStatus() throws Exception{
-//        List<TUserEntity> tUserEntityList = tUserService.getDriverByStatus("active");
-//        System.out.println("how many active driver: " + tUserEntityList.size());
-//    }
-//
-//    @Test
-//    public void testGetTravelOrderByCondition() throws Exception{
-//        List<TTravelOrderEntity> tTravelOrderEntityList = tAirportRideDao.getTravelOrderEntityByType("hitch hike");
-//        TDriverEntity tDriverEntity = tDriverDao.getTDriverEntity(28);
-//        tDriverEntity.setAssignedTripId((long)1);
-//        System.out.println("how many: " + tTravelOrderEntityList.size());
-//    }
-//
-//    @Test
-//    public void testPushOrder() throws Exception{
-//        TTravelOrderEntity tTravelOrderEntity = tTravelOrderDao.getTravelOrders(28);
-//        System.out.println("how many: ");
-//    }
-//
-//    @Test
-//    public void testAutoAssign() throws Exception{
-//        generateTask.setRole("deliveryman");
-//
-//        generateTask.setErrandType("pickup dropoff");
-//        autoDispatchTaskThread.run();
-//        System.out.println("how many: ");
-//    }
-//
-//    @Test
-//    public void testECigaPagination() throws Exception{
-//        List<TECigaretteEntity> teCigaretteEntityList = teCigaretteDao.getTEcigaEntitySorted("salesVol", 3, 6);
-//        System.out.println(teCigaretteEntityList.size());
-//    }
+    @Test
+    public void testGetAllBusinessTypes() throws Exception{
+        List<String> businessTypeList = tBusinessService.getAllBusinessTypes();
+        System.out.println("test hahah: " + businessTypeList.size());
+    }
+
+    @Test
+    public void testGetUsers() throws Exception{
+        Long id = tUserDao.getUserId("oliverxu0205@gmail.com");
+        System.out.println("test hahah: " + id);
+    }
+
+    @Test
+    public void testLoginToken() throws Exception{
+        List<Object> userInfoEntity = tUserService.getUserInfo("xuzhe3", "950205", "driver");
+        String token = "";
+        if (userInfoEntity != null) {
+            Object[] resultUserInfo = (Object[]) userInfoEntity.get(0);
+            Long userId = Long.parseLong(String.valueOf(resultUserInfo[0]));
+            token = JWTUtils.createToken(userId);
+            Map<String, Claim> claims = jwtUtils.verifyToken(token);
+        }
+
+    }
+
+    @Test
+    public void testGetUserId() throws Exception{
+        Long userId = tUserDao.getUserId("oliverxu0205@gmail.com");
+        System.out.println("your userId: " + userId);
+    }
+
+    @Test
+    public void testShoppingCartItems() throws Exception{
+        int cartSize = tShoppingCartService.getCartItem(1).size();
+        System.out.println("my shopping cart size: " + cartSize);
+    }
+
+    @Test
+    public void testEmail() throws Exception{
+//        List<Object> objectList = new ArrayList<>();
+//        TTotalOrderEntity tTotalOrderEntity = tTotalOrderDao.getTTotalOrderEntity(2);
+//        objectList.add(tTotalOrderEntity);
+        List<Long> orderIds = new ArrayList<>();
+        long l1 = 25;
+        long l2= 26;
+        orderIds.add(l1);
+        orderIds.add(l2);
+//        TTotalOrderEntity tTotalOrderEntity = tTotalOrderDao.getTTotalOrderEntity(l1);
+        System.out.println("sent email: " + storeEmailService.sendConfirmationEmail(orderIds));
+
+    }
+
+    @Test
+    public void testLogisticsOrder() throws Exception{
+        List<Long> orderIds = new ArrayList<>();
+        orderIds.add(new Long(25));
+        orderIds.add(new Long(26));
+        tLogisticsOrderService.addShippingOrders(orderIds,1);
+
+    }
+
+    @Test
+    public void testGetTAirportInfoByCondition() throws Exception{
+        List<TAirportInfoEntity> tAirportInfoEntityList = tAirportRideDao.getTAirportEntityByCondition("", "");
+        System.out.println("airportinfo number: " + tAirportInfoEntityList.size());
+
+    }
+
+    @Test
+    public void testGetDriverByStatus() throws Exception{
+        List<TUserEntity> tUserEntityList = tUserService.getDriverByStatus("active");
+        System.out.println("how many active driver: " + tUserEntityList.size());
+    }
+
+    @Test
+    public void testGetTravelOrderByCondition() throws Exception{
+        List<TTravelOrderEntity> tTravelOrderEntityList = tAirportRideDao.getTravelOrderEntityByType("hitch hike");
+        TDriverEntity tDriverEntity = tDriverDao.getTDriverEntity(28);
+        tDriverEntity.setAssignedTripId((long)1);
+        System.out.println("how many: " + tTravelOrderEntityList.size());
+    }
+
+    @Test
+    public void testPushOrder() throws Exception{
+        TTravelOrderEntity tTravelOrderEntity = tTravelOrderDao.getTravelOrders(28);
+        System.out.println("how many: ");
+    }
+
+    @Test
+    public void testAutoAssign() throws Exception{
+        generateTask.setRole("deliveryman");
+
+        generateTask.setErrandType("pickup dropoff");
+        autoDispatchTaskThread.run();
+        System.out.println("how many: ");
+    }
+
+    @Test
+    public void testECigaPagination() throws Exception{
+        List<TECigaretteEntity> teCigaretteEntityList = teCigaretteDao.getTEcigaEntitySorted("salesVol", 3, 6);
+        System.out.println(teCigaretteEntityList.size());
+    }
 
 
 }
